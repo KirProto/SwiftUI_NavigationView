93fde496d85e468acb739cf01bd9333b9cf44742
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 12:33:48 +0500

    Initial Commit

---
+++ Yan_SwiftUI_NavigationView/Yan_SwiftUI_NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_NavigationViewApp.swift
+//  Yan_SwiftUI_NavigationView
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
